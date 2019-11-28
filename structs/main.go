70abceaeb97c9346ea7bf0200f07f948182ade0a
husnutapan <husnu.tapan@gmail.com>
2019-11-29 01:40:42 +0300

    embedded structs definition

---
+++ structs/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	age       int
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func main() {
+	var person2 person
+	person2.firstName = "test"
+	person2.lastName = "test"
+	fmt.Println(person2)
+
+	husnu := person{
+		firstName: "husnu",
+		lastName:  "tapan",
+		age:       25,
+		contact: contactInfo{
+			email:   "test@gmail.com",
+			zipCode: 134,
+		},
+	}
+	husnu.updateName("husnu1")
+	husnu.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
